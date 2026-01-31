1c41501f5ce5ce6813d1061366e0c6da45ddc69c
Gary Smailes <garysmailes@gmail.com>
2026-01-31 10:58:23 +0100

    Add per-user uniqueness validation for SavedItem url scoped to user

--- app/models/saved_item.rb
+++ app/models/saved_item.rb
@@ -3,6 +3,7 @@ class SavedItem < ApplicationRecord
 
   validates :url, presence: true
   validates :user, presence: true
+  validates :url, uniqueness: { scope: :user_id }
 
   scope :for_user, ->(user) { where(user: user) }
 end
