782d361a7487ca19bcd63364de99d40790676d45
Gary Smailes <garysmailes@gmail.com>
2026-01-31 10:36:09 +0100

    Add mobile-first homepage URL submission form to create a Saved Item

--- db/migrate/20260131091254_add_foreign_key_to_saved_items_user.rb
+++ db/migrate/20260131091254_add_foreign_key_to_saved_items_user.rb
@@ -1,5 +1,7 @@
 class AddForeignKeyToSavedItemsUser < ActiveRecord::Migration[8.1]
-   def change
+  def change
+    return if foreign_key_exists?(:saved_items, :users)
+
     add_foreign_key :saved_items, :users
   end
 end
