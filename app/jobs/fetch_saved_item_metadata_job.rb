b5ab0570d717ca73aa6417f21860ddef4e74c76f
Gary Smailes <garysmailes@gmail.com>
2026-01-31 11:37:05 +0100

    Implement non-blocking best-effort metadata fetch to populate fetched_title

--- app/jobs/fetch_saved_item_metadata_job.rb
+++ app/jobs/fetch_saved_item_metadata_job.rb
@@ -5,6 +5,9 @@ class FetchSavedItemMetadataJob < ApplicationJob
     saved_item = SavedItem.find_by(id: saved_item_id)
     return unless saved_item
 
+    # Best-effort enrichment: don't refetch if we already have a title.
+    return if saved_item.fetched_title.present?
+
     SavedItemMetadataFetcher.call(saved_item)
   end
 end
