dbfb89e691a92c850aed3e7f9c005bb773b7f9b5
Gary Smailes <garysmailes@gmail.com>
2026-01-31 10:17:38 +0100

    Add SavedItem validations for presence of url and user

--- app/models/saved_item.rb
+++ app/models/saved_item.rb
@@ -2,7 +2,7 @@ class SavedItem < ApplicationRecord
   belongs_to :user
 
   validates :url, presence: true
-  validates :state, presence: true
+  validates :user, presence: true
 
   scope :for_user, ->(user) { where(user: user) }
 end
