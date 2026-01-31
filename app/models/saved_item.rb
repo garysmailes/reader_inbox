487a2c10fbc62251ffb64605529c208d96f2d630
Gary Smailes <garysmailes@gmail.com>
2026-01-31 10:16:59 +0100

    Add SavedItem state definition with initial state Unread

--- app/models/saved_item.rb
+++ app/models/saved_item.rb
@@ -2,6 +2,7 @@ class SavedItem < ApplicationRecord
   belongs_to :user
 
   validates :url, presence: true
+  validates :state, presence: true
 
   scope :for_user, ->(user) { where(user: user) }
 end
